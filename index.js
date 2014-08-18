4e371a48ba607c15ff6e62abf835dfbba35ae9fe
kaelzhang <i@kael.me>
2014-08-18 22:27:05 +0800

    in progress

--- index.js
+++ index.js
@@ -5,50 +5,71 @@ exports.stringify = stringify;
 exports.strip = strip;
 exports.clean = clean;
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Modified from Douglas Crockford's JSON2: https://github.com/douglascrockford/JSON-js
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function f(n) {
-  // Format integers to have at least two digits.
+
+// Format integers to have at least two digits.
+function format_int(n) {
   return n < 10 ? '0' + n : n;
 }
 
 
-var cx,
-  escapable,
-  gap,
-  indent,
-  meta,
-  rep;
+var escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
+// table of character substitutions
+var meta = {
+  '\b': '\\b',
+  '\t': '\\t',
+  '\n': '\\n',
+  '\f': '\\f',
+  '\r': '\\r',
+  '"' : '\\"',
+  '\\': '\\\\'
+};
 
-
-function quote(string) {
-
-  // If the string contains no control characters, no quote characters, and no
-  // backslash characters, then we can safely slap some quotes around it.
-  // Otherwise we must also replace the offending characters with safe escape
-  // sequences.
-
+function escape (string) {
   escapable.lastIndex = 0;
-  return escapable.test(string) ? '"' + string.replace(escapable, function(a) {
+  if (!escapable.test(string)) {
+    return string;
+  }
+
+  return string.replace(escapable, function(a) {
     var c = meta[a];
-    return typeof c === 'string' ? c : '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
-  }) + '"' : '"' + string + '"';
+    return typeof c === 'string' 
+      ? c 
+      : '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+  });
 }
 
 
-function str(key, holder) {
+function quote(string) {
+  // Escape no control characters, no quote characters, 
+  // and no backslash characters, 
+  // then we can safely slap some quotes around it.
+  return '"' + escape(string) + '"';
+}
+
+
+function is_array (subject) {
+  return Object.prototype.toString.apply(subject) === '[object Array]';
+}
+
+
+function str(key, holder, replacer) {
 
   // Produce a string from holder[key].
 
-  var i, // The loop counter.
-    k, // The member key.
-    v, // The member value.
-    length,
-    mind = gap,
-    partial,
-    value = holder[key];
+  // var i, // The loop counter.
+  //   k, // The member key.
+  //   v, // The member value.
+  //   length,
+  //   mind = gap,
+  //   partial,
+  //   value = holder[key];
+  var value = holder[key];
 
   // If the value has a toJSON method, call it to obtain a replacement value.
-
   if (value && typeof value === 'object' &&
     typeof value.toJSON === 'function') {
     value = value.toJSON(key);
@@ -56,21 +77,16 @@ function str(key, holder) {
 
   // If we were called with a replacer function, then call the replacer to
   // obtain a replacement value.
-
-  if (typeof rep === 'function') {
-    value = rep.call(holder, key, value);
+  if (typeof replacer === 'function') {
+    value = replacer.call(holder, key, value);
   }
-
-  // What happens next depends on the value's type.
 
   switch (typeof value) {
     case 'string':
       return quote(value);
 
     case 'number':
-
       // JSON numbers must be finite. Encode non-finite numbers as null.
-
       return isFinite(value) ? String(value) : 'null';
 
     case 'boolean':
@@ -101,7 +117,7 @@ function str(key, holder) {
 
       // Is the value an array?
 
-      if (Object.prototype.toString.apply(value) === '[object Array]') {
+      if (is_array(value)) {
 
         // The value is an array. Stringify every element. Use null as a placeholder
         // for non-JSON values.
@@ -155,19 +171,9 @@ function str(key, holder) {
   }
 }
 
-// If the JSON object does not yet have a stringify method, give it one.
 
-var escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
-var meta = { // table of character substitutions
-  '\b': '\\b',
-  '\t': '\\t',
-  '\n': '\\n',
-  '\f': '\\f',
-  '\r': '\\r',
-  '"': '\\"',
-  '\\': '\\\\'
-};
-
+// @param {function()|Array} replacer
+// @param {string|number} space
 function stringify (value, replacer, space) {
 
   // The stringify method takes a value and an optional replacer, and an optional
@@ -176,40 +182,30 @@ function stringify (value, replacer, space) {
   // A default replacer method can be provided. Use of the space parameter can
   // produce text that is more easily readable.
 
-  var i;
-  gap = '';
-  indent = '';
-
   // If the space parameter is a number, make an indent string containing that
   // many spaces.
 
+  var i;
+  var indent = '';
   if (typeof space === 'number') {
     for (i = 0; i < space; i += 1) {
       indent += ' ';
     }
 
-    // If the space parameter is a string, it will be used as the indent string.
-
+  // If the space parameter is a string, it will be used as the indent string.
   } else if (typeof space === 'string') {
     indent = space;
   }
 
   // If there is a replacer, it must be a function or an array.
   // Otherwise, throw an error.
-
-  rep = replacer;
-  if (replacer && typeof replacer !== 'function' &&
-    (typeof replacer !== 'object' ||
-      typeof replacer.length !== 'number')) {
+  if (replacer && typeof replacer !== 'function' && !is_array(replacer)) {
     throw new Error('JSON.stringify');
   }
 
   // Make a fake root object containing our value under the key of ''.
   // Return the result of stringifying the value.
-
-  return str('', {
-    '': value
-  });
+  return str('', {'': value}, replacer, '');
 };
 
 
