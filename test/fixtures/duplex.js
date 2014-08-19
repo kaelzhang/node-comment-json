6f15d7aa89c0b3afe7b113768ad611ce393e680e
kaelzhang <i@kael.me>
2014-08-20 02:20:38 +0800

    fixes test cases

--- test/fixtures/duplex.js
+++ test/fixtures/duplex.js
@@ -3,5 +3,5 @@ module.exports = {
   '// a': [['// comment top a'], ['// comment right a']],
   b: 2,
   '// b': [['/* comment top b */']],
-  '// c': [['// comment c']]
+  '// c': '// comment c'
 };
